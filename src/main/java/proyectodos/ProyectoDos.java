713c9eaae587e00b09a469842abe81774a3b3d55
IanEsco632 <ianescobarcolegio@gmail.com>
2024-11-27 12:49:55 -0500

    Update ProyectoDos.java

--- src/main/java/proyectodos/ProyectoDos.java
+++ src/main/java/proyectodos/ProyectoDos.java
@@ -11,7 +11,7 @@ package proyectodos;
 public class ProyectoDos {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! gg");
         System.out.print("José María Sandoval");
         System.out.print(19);
         System.out.print("Semestre Dos");
