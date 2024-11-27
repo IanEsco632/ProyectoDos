d71fa6391af2530b504a4ebd7eb594ec1ada90c7
Josema3l <josema19082005@gmail.com>
2024-11-27 13:01:50 -0500

    Actualizar ProyectoDos.java

--- src/main/java/proyectodos/ProyectoDos.java
+++ src/main/java/proyectodos/ProyectoDos.java
@@ -11,7 +11,7 @@ package proyectodos;
 public class ProyectoDos {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! gg");
+        System.out.println("Hello World! gg IAN PUTO");
         System.out.print("José María Sandoval hola");
         System.out.print(19);
         System.out.print("Semestre Dos");
